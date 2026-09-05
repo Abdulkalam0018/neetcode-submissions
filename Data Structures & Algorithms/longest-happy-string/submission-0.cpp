eaa38368d2956328e596a3a0ce7ed94b26780028
Abdul kalam <166041376+Abdulkalam0018@users.noreply.github.com>
2026-09-05 19:50:55 +0530

    Add: longest-happy-string - submission-0

---
+++ Data Structures & Algorithms/longest-happy-string/submission-0.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string longestDiverseString(int a, int b, int c) {
+        priority_queue<pair<int, char>> pq;
+        
+        if (a > 0) pq.push({a, 'a'});
+        if (b > 0) pq.push({b, 'b'});
+        if (c > 0) pq.push({c, 'c'});
+        
+        string ans = "";
+        
+        while (!pq.empty()) {
+            auto [count1, char1] = pq.top();
+            pq.pop();
+            
+            int n = ans.length();
+            if (n >= 2 && ans[n - 1] == char1 && ans[n - 2] == char1) {
+                if (pq.empty()) {
+                    break;
+                }
+                
+                auto [count2, char2] = pq.top();
+                pq.pop();
+                
+                ans += char2;
+                if (--count2 > 0) {
+                    pq.push({count2, char2});
+                }
+        
+                pq.push({count1, char1});
+            } else {
+                ans += char1;
+                if (--count1 > 0) {
+                    pq.push({count1, char1});
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
