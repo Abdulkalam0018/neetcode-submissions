8b7a910172dab5efa64042972e7bf51b8d5491a3
Abdul kalam <166041376+Abdulkalam0018@users.noreply.github.com>
2026-09-05 22:02:34 +0530

    Add: ipo - submission-1

---
+++ Data Structures & Algorithms/ipo/submission-1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital)
+    {
+        int n=profits.size();
+        using ii=pair<int,int>;
+
+        
+        vector<ii>v;
+        for(int i=0;i<n;i++)
+        {
+            v.push_back({capital[i],profits[i]});
+        }
+        sort(v.begin(),v.end());
+        priority_queue<int>pq;
+        int i=0;
+        while(k--)
+        {
+            
+            while(i<n && v[i].first<=w)
+            {
+                pq.push(v[i].second);
+                i++;
+            }
+            if(pq.empty()) return w;
+            w+=pq.top();
+            pq.pop();
+
+        }
+        return w;
+    }
+};
